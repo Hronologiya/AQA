0176553a07e57e93ed08bf7b113d11b8b465a7d3
Yevhen Hanaha <ganagaz@gmail.com>
2024-09-17 00:10:30 +0100

    Adding tests to homework.

---
+++ lesson_19_1/test_mars_rover.py
@@ -0,0 +1,18 @@
+
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_photos_exist():
+    photo1_path = os.path.join(CURRENT_DIR, 'mars_photo1.jpg')
+    photo2_path = os.path.join(CURRENT_DIR, 'mars_photo2.jpg')
+
+    assert os.path.exists(photo1_path), f"File {photo1_path} not found"
+    assert os.path.exists(photo2_path), f"File {photo2_path} not found"
+
+
+def test_server_response_exists():
+    response_path = os.path.join(CURRENT_DIR, 'server_response.json')
+
+    assert os.path.exists(response_path), f"File {response_path} not found"
