9aa61b37c3e40859b4fed81ed7417de622405e41
Yevhen Hanaha <ganagaz@gmail.com>
2024-10-06 15:54:07 +0100

    Added test class

--- lesson_24/test_cars.py
+++ lesson_24/test_cars.py
@@ -7,11 +7,14 @@ import logging
 
 BASE_URL: str = "http://127.0.0.1:8080"
 
-# Login configuration
 logging.basicConfig(filename='test_search.log', level=logging.INFO)
+
 
 @pytest.fixture(scope="session", autouse=True)
 def authorization() -> Session:
+    """
+    Fixture for authorization of a client. Rotates a session with an authorization token.
+    """
     auth = HTTPBasicAuth('test_user', 'test_pass')
 
     try:
@@ -28,33 +31,48 @@ def authorization() -> Session:
     current_session.headers.update({'Authorization': 'Bearer ' + access_token})
 
     logging.info("Authenticated successfully")
-
     return current_session
 
-@pytest.mark.parametrize("sort_by, limit", [
-    ("price", 5),
-    ("year", 10),
-    ("brand", 15),
-    ("engine_volume", 20),
-    (None, 25),
-])
-def test_get_cars(authorization: Session, sort_by, limit):
-    params = {}
-    if sort_by:
-        params["sort_by"] = sort_by
-    if limit:
-        params["limit"] = limit
 
-    logging.info(f"Testing GET /cars with sort_by={sort_by} and limit={limit}")
+class TestAuthorization:
 
-    try:
-        response: Response = authorization.get(f"{BASE_URL}/cars", params=params)
-        response.raise_for_status()
-    except Exception as e:
-        logging.error(f"Failed to get cars: {e}")
-        pytest.fail("Failed to get cars")
+    def test_authorization_success(self, authorization: Session):
+        """
+        Verifies that the authorization process is successful.
+        """
+        logging.info("Authorization successful. Session obtained.")
+        assert_that(authorization.headers).contains_key('Authorization')
 
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-    if limit:
-        assert len(response.json()) == limit
+
+class TestCarAPI:
+
+    @pytest.mark.parametrize("sort_by, limit", [
+        ("price", 5),
+        ("year", 10),
+        ("brand", 15),
+        ("engine_volume", 20),
+        (None, 25),
+    ])
+    def test_get_cars(self, authorization: Session, sort_by, limit):
+        """
+        Checks the search for cars with different sorting and delineation parameters.
+        """
+        params = {}
+        if sort_by:
+            params["sort_by"] = sort_by
+        if limit:
+            params["limit"] = limit
+
+        logging.info(f"Testing GET /cars with sort_by={sort_by} and limit={limit}")
+
+        try:
+            response: Response = authorization.get(f"{BASE_URL}/cars", params=params)
+            response.raise_for_status()
+        except Exception as e:
+            logging.error(f"Failed to get cars: {e}")
+            pytest.fail("Failed to get cars")
+
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+        if limit:
+            assert len(response.json()) == limit
