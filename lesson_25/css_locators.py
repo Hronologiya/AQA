b4f7757ff6470c0f89ebb82c9e70337f5af5062e
Yevhen Hanaha <ganagaz@gmail.com>
2024-10-16 10:26:08 +0100

    css locators add

--- lesson_25/css_locators.py
+++ lesson_25/css_locators.py
@@ -4,10 +4,10 @@ css_locator_3: str = "button[appscrollto='aboutSection']" # top button about
 css_locator_4: str = "button[appscrollto='contactsSection']" # top button contacts
 css_locator_5: str = "div>button[class='btn btn-outline-white header_signin']" # top button sign
 css_locator_6: str = "section[class='section hero']" # top of the site
-css_locator_7: str = "div[class='section about']" # middle part of the site
+css_locator_7: str = "div[class='section about']" # middle part of the site section about
 css_locator_8: str = "div[class='section contacts']" # contacts section
 css_locator_9: str = "button[class$='btn-primary']" # sign up button at the top of the site
-css_locator_10: str = "*[class^='hero']" # Upper block
+css_locator_10: str = "*[class^='hero']" # .//*[contains(@class, 'hero')]
 css_locator_11: str = "span[class$='facebook']" # Social network button facebook
 css_locator_12: str = "span[class$='telegram']" # Social network button telegram
 css_locator_13: str = "span[class$='youtube']" # Social network button youtube
